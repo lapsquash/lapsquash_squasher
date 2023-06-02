1d51ecec1f0fcdfa48acb7e4d5625a8ab61644a6
wappon-28-dev <wappon_28_dev@ruru.be>
2023-06-03 03:27:11 +0900

    ✨ oclif 追加

---
+++ squasher/src/commands/test/index.ts
@@ -0,0 +1,30 @@
+import { Args, Command, Flags } from "@oclif/core";
+
+export default class Test extends Command {
+  static description = "Say hello";
+
+  static examples = [
+    `$ oex hello friend --from oclif
+hello friend from oclif! (./src/commands/hello/index.ts)
+`,
+  ];
+
+  static flags = {
+    from: Flags.string({
+      char: "f",
+      description: "Who is saying hello",
+      required: true,
+    }),
+  };
+
+  static args = {
+    person: Args.string({
+      description: "Person to say hello to",
+      required: true,
+    }),
+  };
+
+  async run(): Promise<void> {
+    this.log("test");
+  }
+}
