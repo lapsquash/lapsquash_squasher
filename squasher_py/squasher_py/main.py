24479db6039953fd78b2c557dbb3ccf991b5531c
wappon-28-dev <wappon_28_dev@ruru.be>
2023-07-27 15:37:53 +0900

    ⚗️ カメラの映像のpHashをフレーム毎に取る
    ref: #2

--- squasher_py/squasher_py/main.py
+++ squasher_py/squasher_py/main.py
@@ -1,11 +1,25 @@
-from ultralytics import YOLO
+from __future__ import print_function, unicode_literals
 
-# Load a model
-model = YOLO("yolov8n.yaml")  # build a new model from scratch
-model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+import cv2
+from imagehash import average_hash
+from PIL import Image
 
-# Use the model
-model.train(data="coco128.yaml", epochs=3)  # train the model
-metrics = model.val()  # evaluate model performance on the validation set
-results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
-path = model.export(format="onnx")  # export the model to ONNX format
+capture = cv2.VideoCapture(0)
+
+f = 0
+
+while True:
+    ret, frame = capture.read()
+    hash = average_hash(Image.fromarray(frame))
+
+    f += 1
+    h = int(str(hash), base=16)
+    t = f / 30
+
+    print(f"{f} ({t:.2f}s)\t{hash}\t->\t{h}", end="\r")
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
