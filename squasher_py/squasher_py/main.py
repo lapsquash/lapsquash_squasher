7ce68e055c6942b486495abd5505ceefc7cf2552
wappon28dev <wappon_28_dev@ruru.be>
2023-07-31 01:10:34 +0900

    🐛 カメラWidgetとFPSの調整

--- squasher_py/squasher_py/main.py
+++ squasher_py/squasher_py/main.py
@@ -1,73 +1,56 @@
 import sys
 
 import cv2
-import numpy as np
 import pyqtgraph as pg
-from imagehash import average_hash
-from PIL import Image
 from PyQt6.QtCore import QTimer
-from PyQt6.QtWidgets import QApplication
 
-capture = cv2.VideoCapture(0)
+CAPTURE = cv2.VideoCapture(0)
+FPS = CAPTURE.get(cv2.CAP_PROP_FPS)
 
-if not capture.isOpened():
-    raise RuntimeError("Could not open camera/file")
-
-
-app = QApplication([])
-win = pg.GraphicsLayoutWidget(
-    show=True,
-    size=(500, 500),
-    title="pyqtgraph example: Plotting",
-)
-
-plot = win.addPlot(title="ads")
-curve = plot.plot(pen="y")
-
-fps = 60
-n_samples = 500
-fo = 1
-data = np.zeros(n_samples)
-
-iter = 0
+frame_index = 0
+image_item = pg.ImageItem()
 
 
 def update():
-    global curve, data, iter, fps, fo, n_samples
+    global image_item, frame_index
+    frame_index += 1
+    _, frame = CAPTURE.read()
+    print(f"{frame_index}\t{(frame_index/FPS):.2f}", end="\r")
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
 
-    ret, frame = capture.read()
-
-    cv2.imshow("camera", frame)
-
-    hash = average_hash(Image.fromarray(frame))
-
-    fo = 0.1 + iter / n_samples
-    t = (1.0 / fps) * iter
-    idx = iter % n_samples
-    data[idx] = int(str(hash), base=16)
-
-    print(f"{iter:.2f}\t{t:.2f}\t{data[idx]}\t{hash}", end="\r")
-    pos = idx + 1 if idx < n_samples else 0
-    curve.setData(np.r_[data[pos:n_samples], data[0:pos]])
-    iter += 1
+    image_item.setImage(frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
-        exit(0)
+        sys.exit()
+    pass
 
 
-timer = QTimer()
-timer.timeout.connect(update)
-timer.start(int(1 / fps * 1000))
+def get_widget_camera():
+    camera = pg.ViewBox()
+    camera.addItem(image_item)
+    camera.setAspectLocked(True)
+    return pg.PlotItem(viewBox=camera)
+
+
+def get_win():
+    win = pg.GraphicsLayoutWidget()
+
+    win.addItem(get_widget_camera())
+
+    return win
+
 
 if __name__ == "__main__":
-    if sys.flags.interactive == 1:
-        raise RuntimeError("This script is not meant to be run interactively.")
+    app = pg.mkQApp()
+    win = get_win()
 
-    instance = QApplication.instance()
+    timer = QTimer()
+    timer.timeout.connect(update)
+    timer.start()
 
-    if instance is None:
-        raise RuntimeError("No QApplication instance found.")
+    win.show()
+    app.exec()
 
-    instance.exec()
-    capture.release()
-    cv2.destroyAllWindows()
+    CAPTURE.release()
+    sys.exit()
