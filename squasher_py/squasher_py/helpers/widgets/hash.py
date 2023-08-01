21555a3357d09d9bae18b39604e9cc5573c2f2ca
wappon28dev <wappon_28_dev@ruru.be>
2023-08-01 16:08:22 +0900

    ✨ HashWidget 完成！

--- squasher_py/squasher_py/helpers/widgets/hash.py
+++ squasher_py/squasher_py/helpers/widgets/hash.py
@@ -9,26 +9,59 @@ from squasher_py.helpers.state import State
 
 
 class HashWidget(Widget):
-    scatterPlotItem = pg.PlotCurveItem()
+    widePlotItem: pg.PlotItem
+    wideScatterPlotItem = pg.PlotCurveItem()
+
+    narrowPlotItem: pg.PlotItem
+    narrowScatterPlotItem = pg.PlotCurveItem()
 
     def __init__(self, state: State) -> None:
         self.state = state
 
     def get(self) -> QWidget:
         widget = pg.GraphicsLayoutWidget()
-        plotItem = pg.PlotItem()
-        plotItem.addItem(self.scatterPlotItem)
+        self.widePlotItem = pg.PlotItem()
+        self.narrowPlotItem = pg.PlotItem()
+
+        self.widePlotItem.addItem(self.wideScatterPlotItem)
+
+        self.narrowPlotItem.addItem(self.narrowScatterPlotItem)
 
         # config
-        plotItem.showGrid(x=True, y=True)
-        plotItem.setTitle("Hash")
-        plotItem.setLabel("left", "Hash")
-        plotItem.setLabel("bottom", "Frame")
-        plotItem.setMouseEnabled(x=True, y=True)
-        plotItem.setYRange(0, 0xFFFFFFFFFFFFFFFF)
+        self.widePlotItem.showGrid(x=True, y=True)
+        self.widePlotItem.setTitle("Hash")
+        self.widePlotItem.setLabel("left", "Hash")
+        self.widePlotItem.setLabel("bottom", "Frame")
+        self.widePlotItem.setMouseEnabled(x=True, y=True)
+        self.widePlotItem.setYRange(0, 0xFFFFFFFFFFFFFFFF)
+        # self.widePlotItem.sigXRangeChanged.connect(
+        #     lambda: self.__onXRangeChanged(),
+        # )
 
-        widget.addItem(plotItem)
+        self.narrowPlotItem.showGrid(x=True, y=True)
+        self.narrowPlotItem.setTitle("Hash")
+        self.narrowPlotItem.setLabel("left", "Hash")
+        self.narrowPlotItem.setLabel("bottom", "Frame")
+        self.narrowPlotItem.setMouseEnabled(x=True, y=True)
+        self.narrowPlotItem.setYRange(0, 0xFFFFFFFFFFFFFFFF)
+
+        widget.addItem(self.widePlotItem, 0, 0)
+        widget.addItem(self.narrowPlotItem, 0, 1)
         return widget
+
+    # def __onRegionChanged(self) -> None:
+    #     self.narrowPlotItem.setXRange(
+    #         *self.wideLineRegionItem.getRegion(),
+    #         padding=0,
+    #     )
+    #     self.narrowPlotItem.setYRange(0, 2**64)
+
+    # def __onXRangeChanged(self) -> None:
+    #     viewBox = self.narrowPlotItem.getViewBox()
+    #     if viewBox is None:
+    #         return
+    #     self.wideLineRegionItem.setRegion(viewBox.viewRange()[0])
+    #     self.narrowPlotItem.setYRange(0, 0xFFFFFFFFFFFFFFFF)
 
     def update(self) -> None:
         __state = self.state
@@ -41,7 +74,12 @@ class HashWidget(Widget):
 
         self.state.hashArr = np.append(self.state.hashArr, hashInt)
 
-        self.scatterPlotItem.setData(
-            x=np.arange(__frameIdx - 1)[-100:],  # なぜか -1
-            y=__hashArr[-100:],
+        self.wideScatterPlotItem.setData(
+            x=np.arange(__frameIdx - 1),  # なぜか -1
+            y=__hashArr,
+        )
+
+        self.narrowScatterPlotItem.setData(
+            x=np.arange(__frameIdx - 1)[-50:],  # なぜか -1
+            y=__hashArr[-50:],
         )
